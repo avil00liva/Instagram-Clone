e001e112c2a6d8b74287e3919217e5293241cede
avil00liva <agustinoli54@gmail.com>
2022-03-28 09:14:38 -0300

    Load screen?

--- src/components/AllFullStories.jsx
+++ src/components/AllFullStories.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoMdClose } from "react-icons/io"
 import { Link } from 'react-router-dom'
 import { usePosts } from '../context/PostsContext'
 import { auth, db } from '../firebase'
 import {FaTrashAlt} from "react-icons/fa"
 import { deleteDoc, doc } from 'firebase/firestore'
+import loaderImg from "../assets/images/spiner.gif"
 
 const AllFullStories = ({allFullStories, setAllFullStories, stories}) => {
+    const [loaded, setLoaded] = useState(false)
     const { isAuth } = usePosts()
     const usuarioId = auth?.currentUser?.uid
 
     const showStorie = ()=>{
         setAllFullStories(false)
+    }
+
+    const loadComplete = ()=>{
+        setLoaded(true)
     }
 
 return (
@@ -21,7 +27,10 @@ return (
           {stories.map((storie, index)=>{
               return (
                 <div className='snap-start min-w-full min-h-screen relative card flex flex-col' key={index}>
-                    <img src={storie.src}  alt="User storie" className=' w-screen h-screen object-cover absolute top-0 left-0'/>
+                    {!loaded && <div className='flex items-center w-screen h-screen object-cover absolute top-0 left-0 z-[100] bg-gray-200'>
+                        <img src={loaderImg} alt="Loader pic" className='w-auto h-auto' />
+                    </div>}
+                    <img src={storie.src} onLoad={loadComplete}  alt="User storie" className=' w-screen h-screen object-cover absolute top-0 left-0'/>
                     <div className='w-full min-h-[30px] z-50 relative px-2 flex items-center justify-between'>
                         <Link to={`/profile/${storie.iduser}`}>
                             <div className='flex items-center justify-start'>

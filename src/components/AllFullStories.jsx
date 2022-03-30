2eed8ab33b3f8c55d6b90622f5fb021d0589ba7f
avil00liva <agustinoli54@gmail.com>
2022-03-30 18:30:06 -0300

    Storie next btn screen 500px

--- src/components/AllFullStories.jsx
+++ src/components/AllFullStories.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-expressions */
 import React, { useState } from 'react'
 import { IoMdClose } from "react-icons/io"
 import { Link } from 'react-router-dom'
@@ -9,6 +10,8 @@ import loaderImg from "../assets/images/spiner.gif"
 
 const AllFullStories = ({allFullStories, setAllFullStories, stories}) => {
     const [loaded, setLoaded] = useState(false)
+    const [storieActual, setStorieActual]=useState(0)
+    const [mix, setMix] = useState(stories)
     const { isAuth } = usePosts()
     const usuarioId = auth?.currentUser?.uid
 
@@ -19,36 +22,44 @@ const AllFullStories = ({allFullStories, setAllFullStories, stories}) => {
     const loadComplete = ()=>{
         setLoaded(true)
     }
+    const nextStorie = ()=>{
+        if(storieActual <= stories.length - 2 ){
+            setStorieActual(storieActual + 1)
+        } else {
+            showStorie()
+        }
+    }
 
 return (
     <>
     {allFullStories &&
-        <div className='w-full min-h-screen bg-[#ddd] dark:bg-black dark:text-white fixed inset-0 scrolling-wrapper storie-scroll z-50 '>
-          {stories.map((storie, index)=>{
-              return (
-                <div className='snap-start min-w-full min-h-screen relative card flex flex-col' key={index}>
+        <div className='hidden quini:block w-full min-h-screen bg-[#ddd] dark:bg-black dark:text-white fixed inset-0 scrolling-wrapper storie-scroll z-50 '>
+                <div className='snap-start min-w-full min-h-screen relative card flex flex-col'>
                     {!loaded && <div className='flex items-center w-screen h-screen object-cover absolute top-0 left-0 z-[100] bg-gray-200'>
                         <img src={loaderImg} alt="Loader pic" className='w-auto h-auto' />
                     </div>}
-                    <img src={storie.src} onLoad={loadComplete}  alt="User storie" className=' w-screen h-screen object-cover absolute top-0 left-0'/>
+                    <img src={stories[storieActual]?.src} onLoad={loadComplete}  alt="User storie" className=' w-screen h-screen object-cover absolute top-0 left-0'/>
                     <div className='w-full min-h-[30px] z-50 relative px-2 flex items-center justify-between'>
-                        <Link to={`/profile/${storie.iduser}`}>
+                        <Link to={`/profile/${stories[storieActual]?.iduser}`}>
                             <div className='flex items-center justify-start'>
-                                <img src={storie.userImg} alt="User avatar" className='w-[40px] h-[40px] rounded-full' />
+                                <img src={stories[storieActual]?.userImg} alt="User avatar" className='w-[40px] h-[40px] rounded-full' />
                                     <div className='ml-2'>
-                                    <h3 className='text-gray-200'>{storie.username}</h3>
-                                    <h4 className='text-gray-300'>{storie.description}</h4>
+                                    <h3 className='text-gray-200'>{stories[storieActual]?.username}</h3>
+                                    <h4 className='text-gray-300'>{stories[storieActual]?.description}</h4>
                                 </div>
                             </div>
                         </Link>
                         <IoMdClose className='relative z-50 w-8 h-8 text-white' onClick={showStorie}/>
                     </div>
-                    {isAuth && storie.iduser === usuarioId
+                    <div className='py-2 px-4 fixed z-[100] right-0 top-20 bg-red-400 font-bold text-white cursor-pointer' onClick={nextStorie}>
+                        Next
+                    </div>
+                    {isAuth && stories[storieActual]?.iduser === usuarioId
                         ?
                         <div className='mt-4 ml-2 w-[50px] text-3xl cursor-pointer font-bold text-red-500 flex-[0.1] items-center dark:text-red-500 z-50 absolute bottom-8 left-4'>
                             <FaTrashAlt className="w-7 h-7 cursor-pointer font-bold text-red-500" onClick={(e) => {
                                 e.stopPropagation()
-                                deleteDoc(doc(db, "stories", storie.id))
+                                deleteDoc(doc(db, "stories", stories[storieActual]?.id))
                             }}/>
                         </div>
                         :
@@ -56,9 +67,45 @@ return (
                     }
                     
                 </div>
-              )
-          })}
         </div>
+        
+    }
+    {allFullStories &&<div className='quini:hidden w-full min-h-screen bg-[#ddd] dark:bg-black dark:text-white fixed inset-0 scrolling-wrapper storie-scroll z-50 '>
+      {stories.map((storie, index)=>{
+          return (
+            <div className='snap-start min-w-full min-h-screen relative card flex flex-col' key={index}>
+                {!loaded && <div className='flex items-center w-screen h-screen object-cover absolute top-0 left-0 z-[100] bg-gray-200'>
+                    <img src={loaderImg} alt="Loader pic" className='w-auto h-auto' />
+                </div>}
+                <img src={storie.src} onLoad={loadComplete}  alt="User storie" className=' w-screen h-screen object-cover absolute top-0 left-0'/>
+                <div className='w-full min-h-[30px] z-50 relative px-2 flex items-center justify-between'>
+                    <Link to={`/profile/${storie.iduser}`}>
+                        <div className='flex items-center justify-start'>
+                            <img src={storie.userImg} alt="User avatar" className='w-[40px] h-[40px] rounded-full' />
+                                <div className='ml-2'>
+                                <h3 className='text-gray-200'>{storie.username}</h3>
+                                <h4 className='text-gray-300'>{storie.description}</h4>
+                            </div>
+                        </div>
+                    </Link>
+                    <IoMdClose className='relative z-50 w-8 h-8 text-white' onClick={showStorie}/>
+                </div>
+                {isAuth && storie.iduser === usuarioId
+                    ?
+                    <div className='mt-4 ml-2 w-[50px] text-3xl cursor-pointer font-bold text-red-500 flex-[0.1] items-center dark:text-red-500 z-50 absolute bottom-8 left-4'>
+                        <FaTrashAlt className="w-7 h-7 cursor-pointer font-bold text-red-500" onClick={(e) => {
+                            e.stopPropagation()
+                            deleteDoc(doc(db, "stories", storie.id))
+                        }}/>
+                    </div>
+                    :
+                    ""
+                }
+                
+            </div>
+          )
+      })}
+    </div>
     }
   </>
   )
--- src/components/InboxStateModal.jsx
+++ src/components/InboxStateModal.jsx
@@ -7,7 +7,7 @@ const InboxStateModal = ({modalShow, setModalShow, photoUrl}) => {
         <div>
             <div className='fixed top-0 left-0 z-[50] w-full min-h-screen bg-black opacity-30'>
             </div>
-            <div className='bg-[#262626] fixed top-[20%] left-[20%] rounded-lg w-[230px] min-h-[250px] z-[60] pt-4 flex flex-col items-center overflow-hidden'>
+            <div className='bg-[#ddd] dark:bg-[#262626] fixed top-[20%] left-[20%] rounded-lg w-[230px] min-h-[250px] z-[60] pt-4 flex flex-col items-center overflow-hidden'>
                 <img src={photoUrl} className="w-[80px] h-[80px] rounded-full" alt="User avatar" />
                 <strong className='py-4 text-base'>Define un estado</strong>
                 <p className='text-gray-500 text-sm pb-4 text-center px-4'>
